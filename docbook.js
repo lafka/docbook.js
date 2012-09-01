2f37688b9c775c51732ce8a7e9e03a095dc968bd
Olav Frengstad <olav@fwt.no>
2012-09-02 01:22:15 +0200

    Alpha 1
     + Add support for fullview
      + Add markdown
      + Add source highlighting (broken atm)
     + Add history support

--- docbook.js
+++ docbook.js
@@ -23,7 +23,7 @@ docbook.model = {
 		var docs = [];
 		var dir  = false;
 
-		for (var i = 0; i < tags.length; i++) { 
+		for (var i = 0; i < tags.length; i++) {
 			if ('.' == tags[i].innerText.substring(0, 1) ||
 			    '/' == tags[i].innerText.substring(0, 1)) {
 				continue;
@@ -32,8 +32,8 @@ docbook.model = {
 			name = tags[i].innerText;
 
 			docs.push({
-				type : dir ? 'collection' : item,
-				link : tags[i].href,
+				type : dir ? 'collection' : 'item',
+				link : tags[i].getAttribute('href'), 
 				name : dir ? name : docbook.model.rewriteName(name)
 			});
 		}
@@ -41,39 +41,181 @@ docbook.model = {
 		return docs;
 	},
 	fetchItem : function (path) {
-		return {};
+		var name  = path.replace(/^.*\/([^/]+)$/, '$1');
+		var ipath = path.replace(/^(.*\/)[^/]+$/, '$1');
+		return {
+			name   : name, 
+			path   : path,
+			parent : ipath,
+			body   : docbook.model.req(path),
+		};
 	},
 	rewriteName : function(text) {
 		text = text.replace(/\.[^.]$/, '');
 		return text.replace(/[_+]/g, '');
 	}
+
 };
 
 docbook.view = {
-	collection : function (container, collection) {
+	collection : function (container, data, controller) {
+		if (0 == data.collection.length) {
+			var error = document.createElement('div');
+			error.className = 'alert alert-error';
+			docbook.view.setTitle("Not found", controller.opts);
+
+			container.innerHTML = "<p class=\"alert alert-warning\">" +
+				"Sorry, the resource your where looking for could not " +
+				"be found.</p>";
+			return;
+		}
+
 		var render = document.createElement('ul');
  		var item   = undefined
- 		var link = undefined
- 
-		for (var i in collection) {
-			item = document.createElement('li');
-			link = document.createElement('a');
-			link.hash = collection[i].link;
-			link.innerText = collection[i].name;
-			item.appendChild(link);
-			render.appendChild(item);
-		}		
 
+		item = docbook.view.addLink(data.parent + " (parent)",
+			data.parent,
+			controller);
+		item.className = 'parent';
+
+		render.appendChild(item);
+
+		for (var i in data.collection) {
+			item = docbook.view.addLink(data.collection[i].name,
+				data.collection[i].link,
+				controller);
+			render.appendChild(item);
+		}
+			
+		docbook.view.setTitle(data.name, controller.opts);
 		container.innerHTML = "";
 		container.appendChild(render);
+	},
+	item : function(container, data, controller) {
+		convert = new Markdown.getSanitizingConverter().makeHtml;
+
+		render = document.createElement('div');
+		container.innerHTML = "";
+		var name = data.name.replace(/[_.-]/g, ' ');
+
+		var breadcrumb = document.createElement('ul');
+		breadcrumb.className = "breadcrumb";
+
+		var parts = data.path.split('/');
+		var acc   = '';
+
+		for (var i in parts) {
+			acc += parts[i] + '/';
+			parts[i] += ' <span class="divider">/</span>';
+			breadcrumb.appendChild(docbook.view.addLink(parts[i], acc, controller));
+		}
+		
+		docbook.view.setTitle(name, controller.opts);
+		data.body = docbook.view.normalizeLinebreaks(data.body);
+		render.innerHTML = "<ul class=\"breadcrumb\">" + breadcrumb.innerHTML +
+			"</ul>" + convert(docbook.view.convertCodeBlocks(data.body));
+		container.appendChild(render);
+	},
+	addLink : function (name, href, controller) {
+			item = document.createElement('li');
+			link = document.createElement('a');
+			link.href      = href; 
+			link.innerHTML = name;
+			item.appendChild(link);
+			docbook.view.addEvent(link, "click", function (e) {
+				controller.opts.debug && console.log("click: ", this);
+				e.returnValue = false;
+				controller.route(this.pathname);
+			}, false);
+
+			return item;
+	},
+	filterCollection : function (container, collection, controller) {
+		// Do stuff
+	},
+	setTitle : function(text, opts) {
+		opts = opts || {};
+		opts.debug && console.log("setTitle: " + text);
+		var title = document.getElementById(opts.header_id);
+
+		if (null == title) {
+			return;
+		}
+
+		title.innerText = text;
+	},
+	normalizeLinebreaks : function (str, lineEnd) {
+		lineEnd = lineEnd || '\n';
+		return str
+			.replace(/\r\n/g, lineEnd) // DOS
+			.replace(/\r/g, lineEnd) // Mac
+			.replace(/\n/g, lineEnd); // Unix
+	},
+	wrapCode : function(match, lang, code) {
+		var hl;
+		if (lang) {
+ 			try {
+		//		hl = hljs.highlight(lang, code).value;
+ 			} catch(err) {
+				console.log("failed to highlight");
+			}
+		} else {
+		}
+		hl =  hljs.highlightAuto(code).value;
+		return '<pre><code>' + hl + '</code></pre>';
+	},
+	convertCodeBlocks : function(mdown){
+		var re = /^```\s*(\w+)\s*$([\s\S]*?)^```$/gm;
+		return mdown.replace(re, docbook.view.wrapCode);
+	},
+	addEvent : function(obj, ev, fn, capture) {
+		if (obj.length > 1) {
+			for (i in obj)
+				docbook.view.addEvent(obj[i], ev, fn, capture);
+			return;
+		}
+	
+		if (obj.addEventListener) {
+			obj.addEventListener(ev, fn, capture);
+			return true;
+		} else if (obj.attachEvent){
+			var r = obj.attachEvent("on" + ev, fn);
+			return r;
+		} else {
+			return false;
+		}
+	},
+	removeEvent : function (obj, ev, fn, capture){
+		if (obj.length > 1) {
+			for (i in obj)
+				docbook.view.removeEvent(obj[i], ev, fn, capture);
+			
+			return;
+		}
+
+		if (obj.removeEventListener){
+			obj.removeEventListener(ev, fn, capture);
+			return true;
+		} else if (obj.detachEvent){
+			var r = obj.detachEvent("on" + ev, fn);
+			return r;
+		} else {
+			return false;
+		}
+	},
+	triggerEvent : function (obj, ev, event) {
+		event = event || document.createEvent('Event');
+		event.initEvent(ev, true, true);
+		obj.dispatchEvent(event);
 	}
 };
 
-docbook.controller = function (root, container, opts) {
+docbook.controller = function (container, opts) {
 	this.opts = {
-		debug      : true,
-		header_id  : "page-title",
-		content_id : "page-content"
+		debug       : false,
+		header_id   : "page-title",
+		content_id  : "page-content",
+		path_prefix : "",
 	};
 
 	opts = opts || {};
@@ -84,26 +226,46 @@ docbook.controller = function (root, container, opts) {
 		this.opts[k] = opts[k];
 	}
 
-	this.opts.debug && console.log("Init docbook for" + root);
+	this.opts.repository  = this.opts.repository.replace(/(?:^\/|\/$)/, "");
+	this.opts.path_prefix = this.opts.path_prefix.replace(/(?:^\/|\/$)/, "");
+
+	this.opts.debug && console.log("Init docbook for #" + container);
 
 	if (null == this.doc) {
 		alert("Could not find element: #" + container);
 		return;
 	}
 
-	this.setTitle = function(text) {
-		this.opts.debug && console.log("setTitle: " + text);
-		var title = document.getElementById(this.opts.header_id);
+	this.content = document.getElementById(this.opts.content_id);
 
-		if (null == title) {
-			return;
+	docbook.view.addEvent(window, 'popstate', function (e) {
+		book.route(location.href);
+	});
+
+	this.route = function (path) {
+		if ("" == path) {
+			path = this.opts.repository + "/";
+		} 
+
+		this.opts.debug && console.log("path: ", path);
+
+		if ("/" == path.substr(-1)) {
+			var resource = {
+				name       : path, 
+				path       : path,
+				parent     : path.replace(/\/[^/]+\/?$/, '') + "/",
+				collection : docbook.model.fetchCollection(path),
+			};
+
+			docbook.view.collection(this.content, resource, this);
+			history.pushState({collection: path}, path, path);
+		} else {
+			var doc  = docbook.model.fetchItem(path);
+			docbook.view.item(this.content, doc, this);
 		}
+	};
 
-		title.innerText = text;
+	this.rewriteResource = function (resource) {
+		return resource + ".md";
 	}
-
-	this.setTitle("Docbook.js");
-	var container = document.getElementById(this.opts.content_id);
-
-	render.collection(container, docbook.model.fetchCollection(root));
-} 
+}
